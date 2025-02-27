16aa737c4e79d50ea70d949b147a659facffec94
Amy Seibert <amyeseibert@gmail.com>
2025-02-26 22:15:12 -0700

    banner and search/filter

---
+++ frontend/src/app/(index)/kindness-map/page.tsx
@@ -0,0 +1,37 @@
+export default function () {
+    return (
+        <>
+            <section id="replace-banner" className="text-black m-16 flex items-center justify-center">
+                <img src="/heart-icon.png" className="w-12"/>
+                <h1 className="md:text-2xl text-xl text-center font-bold">Kindness Map</h1>
+            </section>
+
+            <section className="flex justify-center items-center gap-x-[1%]">
+                <div id="search">
+                  <div className="relative w-full">
+                      <svg className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                           height="24" fill="none" viewBox="0 0 24 24">
+                          <path stroke="currentColor" strokeLinecap="round" strokeWidth="2"
+                                d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+                      </svg>
+                      <input type="text"
+                           className="bg-gray-50 border border-gray-900 text-gray-900 text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 p-2.5 pl-10"
+                           placeholder= "Search for Location"/>
+                  </div>
+                </div>
+
+                <div id="filter" className="flex h-full items-center">
+                    <svg className="w-6 h-6 text-gray-900 focus:ring-2 focus:ring-purple-600 focus:border-2 focus:border-purple-600 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M18.796 4H5.204a1 1 0 0 0-.753 1.659l5.302 6.058a1 1 0 0 1 .247.659v4.874a.5.5 0 0 0 .2.4l3 2.25a.5.5 0 0 0 .8-.4v-7.124a1 1 0 0 1 .247-.659l5.302-6.059c.566-.646.106-1.658-.753-1.658Z"/>
+                    </svg>
+                    <p className="text-gray-900 ml-1 focus:text-purple-600">Filter</p>
+                </div>
+            </section>
+
+            <section id="map-placeholder">
+
+            </section>
+        </>
+    )
+}
+
