649f02f0c8cc1e8ee9eff7c4504b66d1ae26c461
Amy Seibert <amyeseibert@gmail.com>
2025-03-02 19:09:37 -0700

    add prop, prop type, and dummy data to suggestion card

--- frontend/src/app/kindness-suggestions/SuggestionCard.tsx
+++ frontend/src/app/kindness-suggestions/SuggestionCard.tsx
@@ -1,7 +1,18 @@
 import {Button} from "flowbite-react";
 
+type SuggestionCardProps = {
+    suggestion: Suggestion
+}
 
-export function SuggestionCard() {
+export type Suggestion = {
+    content: string,
+    date: string
+}
+
+export function SuggestionCard(props: SuggestionCardProps) {
+    let {suggestion} = props;
+    let content = suggestion.content;
+    let date = suggestion.date;
     return (
         <>
             <section className="text-gray-900 p-16 flex flex-col relative w-full h-[65vh]">
@@ -13,14 +24,14 @@ export function SuggestionCard() {
                 </div>
                 <div className="relative z-10 flex flex-col items-center justify-center text-center w-full">
                     <h1 className="md:text-4xl text-2xl font-bold">Today's Act of Kindness</h1>
-                    <p className="text-lg p-10">Placeholder Text</p>
+                    <p className="text-xl p-10">{content}</p>
                     <div className="flex items-center justify-center gap-x-[10%] mt:4 whitespace-nowrap">
                         <Button color={"light"} className="focus:ring-4 focus:ring-purple-700 hover:ring-4 hover:ring-purple-700">Plan to do it!</Button>
                         <Button color={"light"} className="focus:ring-4 focus:purple hover:ring-4 hover:ring-purple-700">Done!</Button>
                     </div>
                 </div>
                 </div>
-                <div className='relative flex md:items-end md:justify-end justify-center mt-auto mr-8'>
+                <div className='relative flex md:items-end md:justify-end justify-center mt-auto mr-8 md:text-xl'>
                     <img src="/heart-icon-cropped.png" className="w-6" alt="heart icon"/>
                     <p>### People plan to do this!</p>
                 </div>
