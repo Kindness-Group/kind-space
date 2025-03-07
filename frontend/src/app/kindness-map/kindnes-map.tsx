c166c5ba4320e9e53ac8c9d422b40261c4843bb3
Chris Davila <cdavila7@cnm.edu>
2025-03-06 17:54:02 -0700

    small fixes

--- frontend/src/app/kindness-map/kindnes-map.tsx
+++ frontend/src/app/kindness-map/kindnes-map.tsx
@@ -5,16 +5,16 @@ import Map from 'react-map-gl/mapbox';
 // import Map from 'react-map-gl/mapbox-legacy';
 
 export function KindnessMap() {
-    return (
-        <Map
-            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
-            initialViewState={{
-                longitude: -106.6500,
-                latitude:  35.0844,
-                zoom: 14
-            }}
-            style={{width: "100vw", height: "100vh"}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
-    );
+	return (
+		<Map
+			mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
+			initialViewState={{
+				longitude: -106.6500,
+				latitude:  35.0844,
+				zoom: 14
+			}}
+			style={{width: "100vw", height: "100vh"}}
+			mapStyle="mapbox://styles/mapbox/streets-v9"
+		/>
+	);
 }
